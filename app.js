fa5e7e796226d4b57e356ffc4c9168364b23528f
Serhiy Korytskyy <korysergey55@gmail.com>
2024-05-07 14:40:48 +0300

    mongoose

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-import contactsRouter from "./routes/contactsRouter.js";
+import contactsRouter from './routes/contactsRouter.js';
 
-const app = express();
+const app = express ();
 
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.json());
+app.use (morgan ('dev'));
+app.use (cors ());
+app.use (express.json ());
 
-app.use("/api/contacts", contactsRouter);
+app.use ('/api/contacts', contactsRouter);
 
-app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
+app.use ((_, res) => {
+  res.status (404).json ({message: 'Route not found'});
 });
 
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
+app.use ((err, req, res, next) => {
+  const {status = 500, message = 'Server error'} = err;
+  res.status (status).json ({message});
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
+app.listen (3000, () => {
+  console.log ('Server is running. Use our API on port: 3000');
 });
+
+const DB_HOST =
+  'mongodb+srv://serhiy:10251025ssS@cluster0.udzwbe2.mongodb.net/my-contacts?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose
+  .connect (DB_HOST)
+  .then (() => {
+    app.listen (3000, () => {
+      console.log ('Server is running. Use our API on port: 3000');
+    });
+    console.log ('Database connected success');
+  })
+  .catch (error => {
+    console.log(error);
+    process.exit(1)
+  });
